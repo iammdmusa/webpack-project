09af23700bde70d17e4336cbdfb08cad5408342c
MD Musa <md.musa@medsolv.com>
2021-07-15 16:47:10 +0200

    basic-webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import "./styles/indext.scss";
+
 const musaShieldRecipe = {
    leatherStrips : 2,
    ironIngot: 1,
    refinedMoonst: 4,
 }
 
+const musaGauntletasRecipe = {
+    ...musaShieldRecipe,
+    leather: 10,
+    refinedMoonst:9
+}
+
+const helloArne = {
+    hello: "Hello",
+}
 console.log(musaShieldRecipe);
+
+console.log(musaGauntletasRecipe, helloArne);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let mode = "development";
+let target = "web";
+if(process.env.NODE_ENV === "production"){
+    mode = "production";
+    target = "browserslist"
+}
+module.exports = {
+    mode: mode,
+    target: target,
+    module: {
+        rules:[
+            {
+                test: /\.(s[ac|c])ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [ new MiniCssExtractPlugin()],
+    devtool: "source-map",
+    devServer: {
+        contentBase: "./dist/",
+        hot: true,
+    }
+}
